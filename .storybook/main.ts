89a649fed5832a728a887247536cee3ce1aabb17
Duarte Monteiro <du5rtemonteiro@gmail.com>
2024-04-05 09:00:40 +0100

    feat: upgrade to storybook 8

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,14 @@
 /** @type{import("@storybook/react-webpack5").StorybookConfig} */
 module.exports = {
     stories: [
-      "../src/**/*.stories.mdx",
+      // "../src/**/*.stories.mdx",
       "../src/**/*.stories.@(js|jsx|ts|tsx)",
     ],
     addons: [
       "@storybook/addon-links",
       "@storybook/addon-essentials",
       "@storybook/addon-react-native-web",
+      "@storybook/addon-webpack5-compiler-babel"
     ],
     framework: {
       name: "@storybook/react-webpack5",
